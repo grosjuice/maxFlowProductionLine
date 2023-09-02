48950fb699d765b600ee4873f29b1ef2069eccab
Justin Villeneuve <villeneuve_99@hotmail.com>
2023-09-02 11:21:57 -0400

    modified the structure of the input file. Therefore, reader method was also modified. Transforms the vertex capacities to capacities on edges inorder to work with a maximum flow problem

--- src/main/java/com/villeneuve/justin/Main.java
+++ src/main/java/com/villeneuve/justin/Main.java
@@ -29,8 +29,10 @@ public class Main {
             e.printStackTrace();
         }
 
+        // find number of vertices and edges in the graph
         int n = Integer.parseInt(scanner.nextLine()); // num of vertices
         int m = Integer.parseInt(scanner.nextLine()); // num of edges
+        String s = scanner.nextLine(); // get rid of ---
         LinkedList<Edge>[] adj = new LinkedList[n];
         Map<String, Edge> map = new HashMap<>();
 
@@ -38,18 +40,69 @@ public class Main {
             adj[i] = new LinkedList<>();
         }
 
+        double[] capacities = new double[n];
+
+        // find capacities of vertices
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+
+            if (line.equals("---")) break;
+
+            String[] lineSplit = line.split(" ");
+
+            int v = Integer.valueOf(lineSplit[0]);
+            double capacity;
+
+            if (lineSplit[1].equals("INFINITY")) {
+                capacity = Double.POSITIVE_INFINITY;
+            } else {
+                capacity = Double.valueOf(lineSplit[1]);
+            }
+
+            capacities[v] = capacity;
+        }
+
+        // find edges
         while (scanner.hasNextLine()) {
             String[] line = scanner.nextLine().split(" ");
             int u = Integer.parseInt(line[0]);
             int v = Integer.parseInt(line[1]);
-            int w = Integer.parseInt(line[2]); // weight (the capacity)
-            Edge e = new Edge(u, v, w, 0);
+            Edge e = new Edge(u, v);
             adj[u].add(e);
             adj[v].add(e);
             map.put(u + " " + v, e);
         }
 
-        return new FlowNetwork(n, m, adj, map);
+        // determine the out degree of each vertex in order to calculate the capacity of each edge
+        int[] outDegrees = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int outDegree = 0;
+
+            for (Edge e : adj[i]) {
+                int from = e.getU();
+                if (from == i) {
+                    outDegree++;
+                }
+            }
+            outDegrees[i] = outDegree;
+        }
+
+        // set capacities of edges
+        for (int i = 0 ; i < n; i++) {
+            for (Edge e : adj[i]) {
+                int from = e.getU();
+                if (from == i) {
+                    if (outDegrees[i] == 0) {
+                        e.setCapacity(capacities[i]);
+                    } else {
+                        e.setCapacity(capacities[i] / outDegrees[i]);
+                    }
+                }
+            }
+        }
+
+        return new FlowNetwork(n, m, adj, map, capacities);
     }
 
     public static void writeFile(String output, String content) {
